484cebc42f46aa27d7799aebedaa4f433d0830ad
Lucas Dias <lhollowwizard@gmail.com>
2023-05-26 22:26:55 -0300

    feat: cleaner config construction

--- input/src/lib.rs
+++ input/src/lib.rs
@@ -10,35 +10,37 @@ impl Config {
     pub fn build(
             mut args: impl Iterator<Item = String>
     ) -> Config {
-        args.next();
+        if env::var("BENCHMARK").is_ok() {
+            println!("Running benchnark.");
+            return Config { benchmark: true, steps: 0, color_option: 0 };
+        }
 
+        args.next();
         let steps = match args.next() {
             Some(arg) => arg.parse().unwrap_or_else(|_| {
-                println!("Couldn't parse argument. Using default steps: 3.");
-                3
+                println!("Couldn't parse steps. Using default steps: 6.");
+                6
             }),
             None => {
-                println!("Using default steps amount: 3.");
-                3
+                println!("Using default steps: 6.");
+                6
             }
         };
 
         let color_option = match args.next() {
             Some(arg) => arg.parse().unwrap_or_else(|_| {
-                println!("Couldn't parse argument. Using default colors: Classic.");
+                println!("Couldn't parse color option. Using default colors: Classic.");
                 1
             }),
             None => {
-                println!("Using default colors amount: Classic.");
+                println!("Using default colors: Classic.");
                 1
             }
         };
 
-        let benchmark = env::var("BENCHMARK").is_ok();
-
         Config {
             steps,
-            benchmark,
+            benchmark: false,
             color_option
         }
     }
